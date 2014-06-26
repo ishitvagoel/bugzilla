ee9679ff328911e98c8e1c9577d0a5333afea8be
Ishitva Goel <ishitva.goel@yahoo.co.in>
2014-06-26 22:33:33 +0530

    Migrated the global.js file

--- js/global.js
+++ js/global.js
@@ -41,18 +41,18 @@ function show_forgot_form( suffix ) {
 }
 
 function hide_forgot_form( suffix ) {
-    var forgot_link = document.getElementById('forgot_link' + suffix);
-    var forgot_form = document.getElementById('forgot_form' + suffix);
-    var login_container = document.getElementById('mini_login_container'
-                                                  + suffix);
-    YAHOO.util.Dom.removeClass(forgot_link, 'bz_default_hidden');
-    YAHOO.util.Dom.addClass(forgot_form, 'bz_default_hidden');
-    YAHOO.util.Dom.removeClass(login_container, 'bz_default_hidden');
+    var forgot_link = Y.one('#forgot_link' + suffix);
+    var forgot_form = Y.one('#forgot_form' + suffix);
+    var login_container = Y.one('#mini_login_container' + suffix);
+    
+    forgot_link.removeClass('bz_default_hidden');
+    forgot_form.addClass('bz_default_hidden');
+    login_container.removeClass('bz_default_hidden');
     return false;
 }
 
 function set_language( value ) {
-    YAHOO.util.Cookie.set('LANG', value,
+    Y.Cookie.set('LANG', value,
     {
         expires: new Date('January 1, 2038'),
         path: BUGZILLA.param.cookie_path
